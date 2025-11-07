72189fbfd854445aa744c3af4b6a825e27d75cd4
FMX <felipemxavier@hotmail.com.br>
2025-11-07 19:35:56 -0300

    to upper case

--- src/formatting/EasyFormatting.java
+++ src/formatting/EasyFormatting.java
@@ -1,4 +1,16 @@
+//  Peça ao usuário que digite uma frase e: Mostre a frase toda em letras maiúsculas. (use toUpperCase())
+
 package formatting;
+import java.util.Scanner;
 
 public class EasyFormatting {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Write a sentence: ");
+        String sentence = scn.nextLine();
+        System.out.println("WOW. NO IT'S TIME TO UPPER CASE!!!!!");
+        String uppercase = sentence.toUpperCase();
+        System.out.println(uppercase);
+        scn.close();
+    }
 }
